4e7809dd84c95055b67563f9f0c213fa4150e28c
Mahad <bsef19a505@pucit.edu.pk>
2022-05-25 00:42:30 +0500

    Main Activity

--- app/src/main/java/com/example/mainactivity/Activity2.java
+++ app/src/main/java/com/example/mainactivity/Activity2.java
@@ -2,13 +2,25 @@ package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
-
+    Button  A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        A = (Button) findViewById(R.id.A);
+        A.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+
+            }
+        });
     }
-}
+    }
