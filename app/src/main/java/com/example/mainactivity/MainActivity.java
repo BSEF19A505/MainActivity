5921d79c3c6f79bf6681af39eda7280a44e6953a
Mahad <bsef19a505@pucit.edu.pk>
2022-05-25 00:08:41 +0500

    Main Activity

---
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener(){
+            @Override
+                public void onClick(View view){
+
+                    Intent intent = new Intent(MainActivity.this,Activity2.class);
+            startActivity(intent);
+        }
+        });
+    }
+}
