80cadd9d6aef8f3a3769658a39a045a497badd4f
Mahad <bsef19a505@pucit.edu.pk>
2022-05-25 18:08:51 +0500

    Latest

--- app/src/main/java/com/example/mainactivity/A.java
+++ app/src/main/java/com/example/mainactivity/A.java
@@ -13,10 +13,92 @@ public class A extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
         img = (ImageView) findViewById(R.id.img);
-    }
-    img.OnClickListener(new OnClickListener{
-        void onClick(View v){
+        Bundle extras = getIntent().getExtras();
+        switch(extras.getChar("alphabet")) {
+            case 'a':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+
+            case 'b':
+                img.setBackgroundResource(R.drawable.b);
+                break;
+
+
+            case 'c':
+                img.setBackgroundResource(R.drawable.c);
+                break;
+
+            case 'd':
+                img.setBackgroundResource(R.drawable.d);
+                break;
+            case 'e':
+                img.setBackgroundResource(R.drawable.e);
+                break;
+
+            case 'f':
+                img.setBackgroundResource(R.drawable.f);
+                break;
+            case 'g':
+                img.setBackgroundResource(R.drawable.g);
+                break;
+            case 'h':
+                img.setBackgroundResource(R.drawable.h);
+                break;
+            case 'i':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'j':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'k':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'l':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'm':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'n':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'o':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'p':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'q':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'r':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 's':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 't':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'u':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'v':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'w':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'x':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'y':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+            case 'z':
+                img.setBackgroundResource(R.drawable.a);
+                break;
+
 
         }
-    });
-}
+}}
